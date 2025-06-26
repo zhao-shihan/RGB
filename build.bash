01105ab3230911cfeb3b058e7e5ce87bc5d7b80b
zhaoshihan <zhao_shihan@foxmail.com>
2025-06-26 15:13:26 +0800

    Update build.bash

--- build.bash
+++ build.bash
@@ -2,46 +2,29 @@ apptainer build \
     --build-arg G4DATA_ARCHIVE_HOST_DIR="$(pwd)/g4data" \
     rgb-base.sif \
     def/rgb-base.def
-# apptainer build \
-#     rgb-tianhe2-base.sif \
-#     def/rgb-tianhe2-base.def &
-apptainer build \
-    rgb-mpich-base.sif \
-    def/rgb-mpich-base.def &
-apptainer build \
-    rgb-openmpi-base.sif \
-    def/rgb-openmpi-base.def &
-wait
 
-# apptainer build \
-#     rgb-tianhe2.sif \
-#     def/rgb-tianhe2.def &&
-#     apptainer build \
-#         --build-arg FROM=rgb-tianhe2.sif \
-#         rgb-tianhe2-slim.sif def/slim.def &
-# apptainer build \
-#     rgb-tianhe2-mt.sif \
-#     def/rgb-tianhe2-mt.def &&
-#     apptainer build \
-#         --build-arg FROM=rgb-tianhe2-mt.sif \
-#         rgb-tianhe2-mt-slim.sif def/slim.def &
-# wait
-for mpi in mpich openmpi; do
+function build {
     apptainer build \
-        --build-arg BASE=rgb-$mpi-base.sif \
-        rgb-$mpi.sif \
+        rgb-$1-base.sif \
+        def/rgb-$1-base.def
+    apptainer build \
+        --build-arg BASE=rgb-$1-base.sif \
+        rgb-$1.sif \
         def/rgb.def &&
         apptainer build \
-            --build-arg FROM=rgb-$mpi.sif \
-            rgb-$mpi-slim.sif \
-            def/slim.def &
+            --build-arg FROM=rgb-$1.sif \
+            rgb-$1-slim.sif \
+            def/slim.def
     apptainer build \
-        --build-arg BASE=rgb-$mpi-base.sif \
-        rgb-$mpi-mt.sif \
+        --build-arg BASE=rgb-$1-base.sif \
+        rgb-$1-mt.sif \
         def/rgb-mt.def &&
         apptainer build \
-            --build-arg FROM=rgb-$mpi-mt.sif \
-            rgb-$mpi-mt-slim.sif \
-            def/slim.def &
-    wait
-done
+            --build-arg FROM=rgb-$1-mt.sif \
+            rgb-$1-mt-slim.sif \
+            def/slim.def
+}
+
+build mpich &
+build openmpi &
+wait
