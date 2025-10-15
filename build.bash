fc1733719eb6dea047e240de5a1192916726d3e3
zhaoshihan <zhao_shihan@foxmail.com>
2025-10-15 17:57:08 +0800

    Update COPYING

--- build.bash
+++ build.bash
@@ -1,3 +1,19 @@
+# RGB image build scripts
+# Copyright (C) 2024-2025  Shihan Zhao
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 apptainer build \
     --build-arg SOURCE_ARCHIVE_HOST_DIR="$(pwd)/src" \
     --build-arg G4DATA_ARCHIVE_HOST_DIR="$(pwd)/g4data" \
